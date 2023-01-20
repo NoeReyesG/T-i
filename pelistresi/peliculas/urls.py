2d0b946b3f84b1a0f15a5a0cd9eeb041e0583b95
NoeReyesG <noe1513@gmail.com>
2023-01-19 22:32:02 -0600

    rutas creadas

--- pelistresi/peliculas/urls.py
+++ pelistresi/peliculas/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("registrarpelicula", views.registrar_pelicula, name="registrarpelicula"),
+    path("registrarpersona", views.registrar_persona, name="registrarpersona")
 ]
--- pelistresi/peliculas/views.py
+++ pelistresi/peliculas/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
+    
     return render(request, "peliculas/index.html")
+
+def registrar_pelicula(request):
+    return render(request, "peliculas/registrar_pelicula.html")
+
+def registrar_persona(request):
+    if request.method == 'POST':
+        name = request.POST["nombre"]
+        year = request.POST["year"]
+        print(name)
+        print(year)
+        return HttpResponseRedirect(reverse("index"))
+    else:    
+        return render(request, "peliculas/registrar_persona.html")
