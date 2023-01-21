ee4f08f50cbad84128e9f82c641af4aa48e712bc
NoeReyesG <noe1513@gmail.com>
2023-01-20 18:28:02 -0600

    borrado de innecesaria base de datos

--- pelistresi/pelistresi/settings.py
+++ pelistresi/pelistresi/settings.py
@@ -78,10 +78,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'movies': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'movies.db',
     }
 }
 
