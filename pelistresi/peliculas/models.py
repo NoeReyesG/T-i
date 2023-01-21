91b7815b98f9ff6f4061fde2a0528162ec48f1fa
NoeReyesG <noe1513@gmail.com>
2023-01-20 18:21:49 -0600

    injesta de datos de todas las peliculas del 2022 completa

--- pelistresi/peliculas/models.py
+++ pelistresi/peliculas/models.py
@@ -1,33 +1,34 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
 
-# Estamos creando una abstracción de una base de datos para peliculas.
-# Los modelos en Django pueden administrar diferentes BD, para el problema solicitado SQLite es suficiente, pero con algo de 
-# configuración, podemos conectar si fuera necesario a alguna otra cómo MySQL, por ejemplo.
-
-# Modelo equivalente a una tabla para peliculas 
 class Movies(models.Model):
-    title = models.CharField( max_length=300, default=None)
-    year = models.IntegerField( default=None)
-
-# Modelo equivalente a una tabla para las personas
+    title = models.TextField()
+    year = models.IntegerField(blank=True, null=True)  
+    
 class People(models.Model):
-    name = models.CharField(max_length = 400)
-    birth = models.IntegerField(blank=True)
+    name = models.TextField()
+    birth = models.IntegerField(blank=True, null=True) 
+    
 
-# Modelo equivalente a una tabla que conecta a las personas con las peliculas en las que actúan o participan.
-class Stars(models.Model):
-    movie_id = models.ForeignKey(Movies, on_delete=models.CASCADE, default=None)
-    person_id = models.ForeignKey(People, on_delete=models.CASCADE, default=None)
-
-#Modelo equivalente a una tabla que conecta a los directores con las peliculas que dirijieron
 class Directors(models.Model):
-    movie_id = models.ForeignKey(Movies, on_delete=models.CASCADE, default=None)
-    person_id = models.ForeignKey(People, on_delete=models.CASCADE, default=None)
+    movie = models.ForeignKey('Movies', on_delete=models.CASCADE, default=None)
+    person = models.ForeignKey('People', on_delete=models.CASCADE, default=None)
+
 
 class Ratings(models.Model):
-    movie_id = models.ForeignKey(Movies, on_delete=models.CASCADE, default=None)
-    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    movie = models.ForeignKey(Movies, on_delete=models.CASCADE, default=None)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
     votes = models.IntegerField()
+
+
+class Stars(models.Model):
+    movie = models.ForeignKey(Movies, on_delete=models.CASCADE, default=None)
+    person = models.ForeignKey(People, on_delete=models.CASCADE, default=None)
 
--- pelistresi/peliculas/views.py
+++ pelistresi/peliculas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from .models import Movies, People, Directors, Ratings, Stars
 # Create your views here.
 
 def index(request):
